a777f97fad3fc5cd4c43a07fda158b43f0ddfdcc
Ankit kumar <iamankitm05@gmail.com>
2025-10-29 22:51:13 +0530

    Initial Commit

---
+++ UnitEase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitEase
+//
+//  Created by Ankit Kumar Ojha on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitEase/UnitEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitEaseApp.swift
+//  UnitEase
+//
+//  Created by Ankit Kumar Ojha on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
